d30e68a9323ada7a08d2b2b7395c35d066b141d7
yunsiechung <yunsie@mit.edu>
2020-07-13 18:25:26 -0400

    New GAV groups for solvation thermo
    Added new groups for solvation thermo GAV. It follows the similar format as
    gas-phase thermo GAV

---
+++ input/solvation/groups/longDistanceInteraction_cyclic.py
@@ -0,0 +1,650 @@
+name = "Long distance interaction correction for cyclic molecule" 
+shortDesc = u"" 
+longDesc = u""" 
+
+""" 
+entry(
+	 index = -1,
+	 label = "R",
+	     group = 
+"""
+1 * R ux
+""",
+	 solute = None, 
+	 shortDesc = u"""""",
+	 longDesc = 
+u"""
+""",
+)
+entry(
+    index = 15,
+    label = "o_OH_OH",
+    group = 
+"""
+1 *1 Cb u0 {2,B} {3,S}
+2 *2 Cb u0 {1,B} {4,S}
+3    O  u0 {1,S} {5,S}
+4    O  u0 {2,S} {6,S}
+5    H  u0 {3,S}
+6    H  u0 {4,S}
+""",
+    solute = SoluteData(
+        S = -0.02678115184720035,
+        B = -0.011243521250034354,
+        E = -0.006950862186513421,
+        L = -0.06985512715255904,
+        A = -0.03257806787105071,
+    ),
+    )
+
+
+entry(
+    index = 15,
+    label = "o_OH",
+    group = 
+"""
+1 *1 Cb u0 {2,S} {3,B}
+2    O  u0 {1,S} {4,S}
+3 *2 Cb u0 {1,B}
+4    H  u0 {2,S}
+""",
+    solute = None,
+)
+
+
+entry(
+    index = 25,
+    label = "aromatic-ortho",
+    group = 
+"""
+1 *1 Cb u0 {2,B}
+2 *2 Cb u0 {1,B}
+""",
+    solute = None,
+)
+
+
+entry(
+    index = 12,
+    label = "o_CH3_CH3",
+    group = 
+"""
+1     C  u0 {3,S} {5,S} {6,S} {7,S}
+2     C  u0 {4,S} {8,S} {9,S} {10,S}
+3  *1 Cb u0 {1,S} {4,B}
+4  *2 Cb u0 {2,S} {3,B}
+5     H  u0 {1,S}
+6     H  u0 {1,S}
+7     H  u0 {1,S}
+8     H  u0 {2,S}
+9     H  u0 {2,S}
+10    H  u0 {2,S}
+""",
+    solute = SoluteData(
+        S = 0.014780476072956066,
+        B = 0.0015698310636721021,
+        E = 0.013734351133301969,
+        L = 0.06662498795605573,
+        A = 0.0021979153169533736,
+    ),
+    )
+
+
+entry(
+    index = 12,
+    label = "o_CH3",
+    group = 
+"""
+1    C  u0 {2,S} {3,S} {4,S} {5,S}
+2 *1 Cb u0 {1,S} {6,B}
+3    H  u0 {1,S}
+4    H  u0 {1,S}
+5    H  u0 {1,S}
+6 *2 Cb u0 {2,B}
+""",
+    solute = None,
+)
+
+
+entry(
+    index = 13,
+    label = "o_CH3_C2H5",
+    group = 
+"""
+1     C  u0 {2,S} {4,S} {6,S} {7,S}
+2     C  u0 {1,S} {8,S} {9,S} {10,S}
+3     C  u0 {5,S} {11,S} {12,S} {13,S}
+4  *2 Cb u0 {1,S} {5,B}
+5  *1 Cb u0 {3,S} {4,B}
+6     H  u0 {1,S}
+7     H  u0 {1,S}
+8     H  u0 {2,S}
+9     H  u0 {2,S}
+10    H  u0 {2,S}
+11    H  u0 {3,S}
+12    H  u0 {3,S}
+13    H  u0 {3,S}
+""",
+    solute = SoluteData(
+        S = 0.010002529195170187,
+        B = -0.018139846562919573,
+        E = 0.030010127060326932,
+        L = 0.12296684213905616,
+        A = 0.007717966868743846,
+    ),
+    )
+
+
+entry(
+    index = 14,
+    label = "o_C2H5_C2H5",
+    group = 
+"""
+1     C  u0 {3,S} {5,S} {7,S} {8,S}
+2     C  u0 {4,S} {6,S} {9,S} {10,S}
+3     C  u0 {1,S} {14,S} {15,S} {16,S}
+4     C  u0 {2,S} {11,S} {12,S} {13,S}
+5  *1 Cb u0 {1,S} {6,B}
+6  *2 Cb u0 {2,S} {5,B}
+7     H  u0 {1,S}
+8     H  u0 {1,S}
+9     H  u0 {2,S}
+10    H  u0 {2,S}
+11    H  u0 {4,S}
+12    H  u0 {4,S}
+13    H  u0 {4,S}
+14    H  u0 {3,S}
+15    H  u0 {3,S}
+16    H  u0 {3,S}
+""",
+    solute = SoluteData(
+        S = 0.00956238840607638,
+        B = -0.0192475818540464,
+        E = 0.012094358886379544,
+        L = 0.08154807733117815,
+        A = 0.008347546517170793,
+    ),
+    )
+
+
+entry(
+    index = 14,
+    label = "o_C2H5",
+    group = 
+"""
+1    C  u0 {2,S} {3,S} {4,S} {5,S}
+2    C  u0 {1,S} {6,S} {7,S} {8,S}
+3 *1 Cb u0 {1,S} {9,B}
+4    H  u0 {1,S}
+5    H  u0 {1,S}
+6    H  u0 {2,S}
+7    H  u0 {2,S}
+8    H  u0 {2,S}
+9 *2 Cb u0 {3,B}
+""",
+    solute = None,
+)
+
+
+entry(
+    index = 9,
+    label = "o_vinyl_CH3",
+    group = 
+"""
+1     C  u0 {4,S} {6,S} {7,S} {8,S}
+2     C  u0 {3,D} {5,S} {9,S}
+3     C  u0 {2,D} {10,S} {11,S}
+4  *2 Cb u0 {1,S} {5,B}
+5  *1 Cb u0 {2,S} {4,B}
+6     H  u0 {1,S}
+7     H  u0 {1,S}
+8     H  u0 {1,S}
+9     H  u0 {2,S}
+10    H  u0 {3,S}
+11    H  u0 {3,S}
+""",
+    solute = SoluteData(
+        S = 0.006976324256955572,
+        B = -0.005011417657232979,
+        E = 0.033144436648075516,
+        L = 0.03225063073572207,
+        A = -3.666035419034662e-05,
+    ),
+    )
+
+
+entry(
+    index = 6,
+    label = "o_vinyl",
+    group = 
+"""
+1    C  u0 {2,D} {3,S} {4,S}
+2    C  u0 {1,D} {5,S} {6,S}
+3 *1 Cb u0 {1,S} {7,B}
+4    H  u0 {1,S}
+5    H  u0 {2,S}
+6    H  u0 {2,S}
+7 *2 Cb u0 {3,B}
+""",
+    solute = None,
+)
+
+
+entry(
+    index = 4,
+    label = "o_MeO_MeO",
+    group = 
+"""
+1     C  u0 {3,S} {7,S} {8,S} {9,S}
+2     C  u0 {4,S} {10,S} {11,S} {12,S}
+3     O  u0 {1,S} {5,S}
+4     O  u0 {2,S} {6,S}
+5  *1 Cb u0 {3,S} {6,B}
+6  *2 Cb u0 {4,S} {5,B}
+7     H  u0 {1,S}
+8     H  u0 {1,S}
+9     H  u0 {1,S}
+10    H  u0 {2,S}
+11    H  u0 {2,S}
+12    H  u0 {2,S}
+""",
+    solute = SoluteData(
+        S = 0.023223169175430908,
+        B = 0.017671377873057576,
+        E = -0.019790203041333203,
+        L = -0.013951387605037458,
+        A = 0.011920672935943632,
+    ),
+    )
+
+
+entry(
+    index = 4,
+    label = "o_MeO",
+    group = 
+"""
+1    C  u0 {2,S} {4,S} {5,S} {6,S}
+2    O  u0 {1,S} {3,S}
+3 *1 Cb u0 {2,S} {7,B}
+4    H  u0 {1,S}
+5    H  u0 {1,S}
+6    H  u0 {1,S}
+7 *2 Cb u0 {3,B}
+""",
+    solute = None,
+)
+
+
+entry(
+    index = 24,
+    label = "p_MeO_MeO",
+    group = 
+"""
+1     C  u0 {3,S} {12,S} {13,S} {14,S}
+2     C  u0 {4,S} {9,S} {10,S} {11,S}
+3     O  u0 {1,S} {5,S}
+4     O  u0 {2,S} {6,S}
+5  *1 Cb u0 {3,S} {7,B}
+6  *2 Cb u0 {4,S} {8,B}
+7     Cb u0 {5,B} {8,B}
+8     Cb u0 {6,B} {7,B}
+9     H  u0 {2,S}
+10    H  u0 {2,S}
+11    H  u0 {2,S}
+12    H  u0 {1,S}
+13    H  u0 {1,S}
+14    H  u0 {1,S}
+""",
+    solute = SoluteData(
+        S = 0.03240653980149021,
+        B = -0.03135831091858667,
+        E = -0.0009747892965889078,
+        L = -0.03181004245942251,
+        A = -0.0016379622274380918,
+    ),
+    )
+
+
+entry(
+    index = 24,
+    label = "p_MeO",
+    group = 
+"""
+1    C  u0 {2,S} {6,S} {7,S} {8,S}
+2    O  u0 {1,S} {3,S}
+3 *1 Cb u0 {2,S} {4,B}
+4    Cb u0 {3,B} {5,B}
+5    Cb u0 {4,B} {9,B}
+6    H  u0 {1,S}
+7    H  u0 {1,S}
+8    H  u0 {1,S}
+9 *2 Cb u0 {5,B}
+""",
+    solute = None,
+)
+
+
+entry(
+    index = 0,
+    label = "aromatic-para",
+    group = 
+"""
+1    Cb u0 {2,B} {3,B}
+2    Cb u0 {1,B} {4,B}
+3 *1 Cb u0 {1,B}
+4 *2 Cb u0 {2,B}
+""",
+    solute = None,
+)
+
+
+entry(
+    index = 7,
+    label = "o_CHO_CH3",
+    group = 
+"""
+1    C  u0 {3,S} {5,S} {6,S} {7,S}
+2    C  u0 {4,S} {8,S} {9,D}
+3 *2 Cb u0 {1,S} {4,B}
+4 *1 Cb u0 {2,S} {3,B}
+5    H  u0 {1,S}
+6    H  u0 {1,S}
+7    H  u0 {1,S}
+8    H  u0 {2,S}
+9    O  u0 {2,D}
+""",
+    solute = SoluteData(
+        S = -0.013677178172325206,
+        B = 0.007257576952284303,
+        E = 0.018750286461340027,
+        L = 0.07143036456514641,
+        A = -0.0007492035582228443,
+    ),
+    )
+
+
+entry(
+    index = 3,
+    label = "o_CHO",
+    group = 
+"""
+1    C  u0 {2,S} {3,S} {4,D}
+2 *1 Cb u0 {1,S} {5,B}
+3    H  u0 {1,S}
+4    O  u0 {1,D}
+5 *2 Cb u0 {2,B}
+""",
+    solute = None,
+)
+
+
+entry(
+    index = 11,
+    label = "o_CHO_MeO",
+    group = 
+"""
+1     C  u0 {3,S} {6,S} {7,S} {8,S}
+2     C  u0 {4,S} {9,S} {10,D}
+3     O  u0 {1,S} {5,S}
+4  *1 Cb u0 {2,S} {5,B}
+5  *2 Cb u0 {3,S} {4,B}
+6     H  u0 {1,S}
+7     H  u0 {1,S}
+8     H  u0 {1,S}
+9     H  u0 {2,S}
+10    O  u0 {2,D}
+""",
+    solute = SoluteData(
+        S = -0.0089962529943208,
+        B = -0.01218789474289042,
+        E = 0.05907577477989655,
+        L = 0.09398704845638405,
+        A = 0.005734925675685355,
+    ),
+    )
+
+
+entry(
+    index = 27,
+    label = "p_MeO_CHO",
+    group = 
+"""
+1     C  u0 {3,S} {8,S} {9,S} {10,S}
+2     C  u0 {4,S} {11,D} {12,S}
+3     O  u0 {1,S} {5,S}
+4  *2 Cb u0 {2,S} {6,B}
+5  *1 Cb u0 {3,S} {7,B}
+6     Cb u0 {4,B} {7,B}
+7     Cb u0 {5,B} {6,B}
+8     H  u0 {1,S}
+9     H  u0 {1,S}
+10    H  u0 {1,S}
+11    O  u0 {2,D}
+12    H  u0 {2,S}
+""",
+    solute = SoluteData(
+        S = 0.01975897101886766,
+        B = -0.021557163850406555,
+        E = 0.04417536557553183,
+        L = 0.0378752660352623,
+        A = -0.0038841882291138774,
+    ),
+    )
+
+
+entry(
+    index = 3,
+    label = "o_CHO_CHO",
+    group = 
+"""
+1    C  u0 {3,S} {5,S} {8,D}
+2    C  u0 {4,S} {6,S} {7,D}
+3 *1 Cb u0 {1,S} {4,B}
+4 *2 Cb u0 {2,S} {3,B}
+5    H  u0 {1,S}
+6    H  u0 {2,S}
+7    O  u0 {2,D}
+8    O  u0 {1,D}
+""",
+    solute = SoluteData(
+        S = -0.012238033664136666,
+        B = 0.01146968036371983,
+        E = -0.015163516643679795,
+        L = 0.1205905757570004,
+        A = 0.008469706274404387,
+    ),
+    )
+
+
+entry(
+    index = 25,
+    label = "p_CHO_CHO",
+    group = 
+"""
+1     C  u0 {3,S} {7,S} {10,D}
+2     C  u0 {4,S} {8,D} {9,S}
+3  *1 Cb u0 {1,S} {5,B}
+4  *2 Cb u0 {2,S} {6,B}
+5     Cb u0 {3,B} {6,B}
+6     Cb u0 {4,B} {5,B}
+7     H  u0 {1,S}
+8     O  u0 {2,D}
+9     H  u0 {2,S}
+10    O  u0 {1,D}
+""",
+    solute = SoluteData(
+        S = -0.026613507719163914,
+        B = 0.004023169206518073,
+        E = 0.0004620237742506726,
+        L = -0.06638512510530889,
+        A = -0.002890071473293455,
+    ),
+    )
+
+
+entry(
+    index = 25,
+    label = "p_CHO",
+    group = 
+"""
+1    C  u0 {2,S} {5,S} {6,D}
+2 *1 Cb u0 {1,S} {3,B}
+3    Cb u0 {2,B} {4,B}
+4    Cb u0 {3,B} {7,B}
+5    H  u0 {1,S}
+6    O  u0 {1,D}
+7 *2 Cb u0 {4,B}
+""",
+    solute = None,
+)
+
+
+entry(
+    index = 22,
+    label = "p_OH_OH",
+    group = 
+"""
+1    Cb u0 {2,B} {3,B}
+2    Cb u0 {1,B} {4,B}
+3 *1 Cb u0 {1,B} {5,S}
+4 *2 Cb u0 {2,B} {6,S}
+5    O  u0 {3,S} {7,S}
+6    O  u0 {4,S} {8,S}
+7    H  u0 {5,S}
+8    H  u0 {6,S}
+""",
+    solute = SoluteData(
+        S = 0.027468144337867144,
+        B = -0.005496938902807853,
+        E = 0.0019888193414746434,
+        L = -0.05446205829820941,
+        A = -0.07983921641389058,
+    ),
+    )
+
+
+entry(
+    index = 22,
+    label = "p_OH",
+    group = 
+"""
+1 *1 Cb u0 {2,B} {4,S}
+2    Cb u0 {1,B} {3,B}
+3    Cb u0 {2,B} {5,B}
+4    O  u0 {1,S} {6,S}
+5 *2 Cb u0 {3,B}
+6    H  u0 {4,S}
+""",
+    solute = None,
+)
+
+
+entry(
+    index = 16,
+    label = "o_OH_MeO",
+    group = 
+"""
+1    C  u0 {2,S} {6,S} {7,S} {8,S}
+2    O  u0 {1,S} {3,S}
+3 *2 Cb u0 {2,S} {4,B}
+4 *1 Cb u0 {3,B} {5,S}
+5    O  u0 {4,S} {9,S}
+6    H  u0 {1,S}
+7    H  u0 {1,S}
+8    H  u0 {1,S}
+9    H  u0 {5,S}
+""",
+    solute = SoluteData(
+        S = -0.04965613522261422,
+        B = 0.008393692811456006,
+        E = -0.04700457916980601,
+        L = -0.1269996191667133,
+        A = -0.13138814618030012,
+    ),
+    )
+
+
+entry(
+    index = 23,
+    label = "p_OH_MeO",
+    group = 
+"""
+1     C  u0 {2,S} {8,S} {9,S} {10,S}
+2     O  u0 {1,S} {3,S}
+3  *2 Cb u0 {2,S} {5,B}
+4     Cb u0 {5,B} {6,B}
+5     Cb u0 {3,B} {4,B}
+6  *1 Cb u0 {4,B} {7,S}
+7     O  u0 {6,S} {11,S}
+8     H  u0 {1,S}
+9     H  u0 {1,S}
+10    H  u0 {1,S}
+11    H  u0 {7,S}
+""",
+    solute = SoluteData(
+        S = 0.038010210443479124,
+        B = -0.028464440952301332,
+        E = -0.016855401977514277,
+        L = -0.044483556060582866,
+        A = -0.02267519373000539,
+    ),
+    )
+
+
+entry(
+    index = 26,
+    label = "p_OH_CHO",
+    group = 
+"""
+1    C  u0 {2,S} {7,D} {8,S}
+2 *2 Cb u0 {1,S} {3,B}
+3    Cb u0 {2,B} {4,B}
+4    Cb u0 {3,B} {5,B}
+5 *1 Cb u0 {4,B} {6,S}
+6    O  u0 {5,S} {9,S}
+7    O  u0 {1,D}
+8    H  u0 {1,S}
+9    H  u0 {6,S}
+""",
+    solute = SoluteData(
+        S = 0.011384135949332113,
+        B = -0.018245103025160365,
+        E = -0.014457614846908903,
+        L = 0.004193269001707425,
+        A = 0.006074340466525844,
+    ),
+    )
+
+
+tree(
+"""
+L1: R
+	L2: aromatic-para
+		L3: p_OH
+			L4: p_OH_CHO
+			L4: p_OH_MeO
+			L4: p_OH_OH
+		L3: p_CHO
+			L4: p_CHO_CHO
+		L3: p_MeO
+			L4: p_MeO_CHO
+			L4: p_MeO_MeO
+	L2: aromatic-ortho
+		L3: o_CHO
+			L4: o_CHO_CHO
+			L4: o_CHO_MeO
+			L4: o_CHO_CH3
+		L3: o_MeO
+			L4: o_MeO_MeO
+		L3: o_vinyl
+			L4: o_vinyl_CH3
+		L3: o_C2H5
+			L4: o_C2H5_C2H5
+		L3: o_CH3
+			L4: o_CH3_C2H5
+			L4: o_CH3_CH3
+		L3: o_OH
+			L4: o_OH_MeO
+			L4: o_OH_OH
+""",
+)
---
+++ input/solvation/groups/longDistanceInteraction_noncyclic.py
@@ -0,0 +1,614 @@
+name = "Long distance interaction correction for non-cyclic molecule" 
+shortDesc = u"" 
+longDesc = u""" 
+
+""" 
+entry(
+	 index = -1,
+	 label = "R",
+	     group = 
+"""
+1 * R ux
+""",
+	 solute = None, 
+	 shortDesc = u"""""",
+	 longDesc = 
+u"""
+""",
+)
+entry(
+    index = 7,
+    label = "CsCs-ST",
+    group = 
+"""
+1 *1 Cs                          u0 {2,S} {3,S} {4,S} {5,S}
+2 *2 Cs                          u0 {1,S} {6,S} {7,S} {8,S}
+3    Cs                          u0 {1,S}
+4    [Cd,Cdd,Ct,Cb,Cbf,O2s,CO,H] u0 {1,S}
+5    [Cd,Cdd,Ct,Cb,Cbf,O2s,CO,H] u0 {1,S}
+6    Cs                          u0 {2,S}
+7    Cs                          u0 {2,S}
+8    [Cd,Cdd,Ct,Cb,Cbf,O2s,CO,H] u0 {2,S}
+""",
+    solute = SoluteData(
+        S = -0.019210180568089597,
+        B = -0.0013718471622547446,
+        E = 0.0012983765111619398,
+        L = -0.017690538552214564,
+        A = 0.012484281438863888,
+    ),
+    )
+
+
+entry(
+    index = 2,
+    label = "CsCs-S",
+    group = 
+"""
+1 *1 Cs                          u0 {2,S} {3,S} {4,S} {5,S}
+2 *2 Cs                          u0 {1,S}
+3    [Cd,Cdd,Ct,Cb,Cbf,O2s,CO,H] u0 {1,S}
+4    [Cd,Cdd,Ct,Cb,Cbf,O2s,CO,H] u0 {1,S}
+5    Cs                          u0 {1,S}
+""",
+    solute = None,
+)
+
+
+entry(
+    index = 1,
+    label = "CsCs",
+    group = 
+"""
+1 *1 Cs u0 {2,S}
+2 *2 Cs u0 {1,S}
+""",
+    solute = None,
+)
+
+
+entry(
+    index = 0,
+    label = "int14_gauche",
+    group = 
+"""
+1 *1 [Cs,O2s,Cd,S2s] u0 {2,S}
+2 *2 Cs              u0 {1,S}
+""",
+    solute = None,
+)
+
+
+entry(
+    index = 8,
+    label = "CsCs-SQ",
+    group = 
+"""
+1 *1 Cs                          u0 {2,S} {3,S} {4,S} {5,S}
+2 *2 Cs                          u0 {1,S} {6,S} {7,S} {8,S}
+3    Cs                          u0 {1,S}
+4    [Cd,Cdd,Ct,Cb,Cbf,O2s,CO,H] u0 {1,S}
+5    [Cd,Cdd,Ct,Cb,Cbf,O2s,CO,H] u0 {1,S}
+6    Cs                          u0 {2,S}
+7    Cs                          u0 {2,S}
+8    Cs                          u0 {2,S}
+""",
+    solute = SoluteData(
+        S = -0.025722222422190723,
+        B = -0.02290965817519166,
+        E = 0.01893162615226219,
+        L = -0.030242000204771458,
+        A = -0.025635166498791887,
+    ),
+    )
+
+
+entry(
+    index = 9,
+    label = "CsCs-TT",
+    group = 
+"""
+1 *1 Cs                          u0 {2,S} {3,S} {4,S} {5,S}
+2 *2 Cs                          u0 {1,S} {6,S} {7,S} {8,S}
+3    Cs                          u0 {1,S}
+4    Cs                          u0 {1,S}
+5    [Cd,Cdd,Ct,Cb,Cbf,O2s,CO,H] u0 {1,S}
+6    Cs                          u0 {2,S}
+7    Cs                          u0 {2,S}
+8    [Cd,Cdd,Ct,Cb,Cbf,O2s,CO,H] u0 {2,S}
+""",
+    solute = SoluteData(
+        S = -0.06266688584244531,
+        B = -0.02173327842634227,
+        E = 0.016274858346373548,
+        L = 0.031905497424796134,
+        A = 0.0056754602502876285,
+    ),
+    )
+
+
+entry(
+    index = 2,
+    label = "CsCs-T",
+    group = 
+"""
+1 *1 Cs                          u0 {2,S} {3,S} {4,S} {5,S}
+2 *2 Cs                          u0 {1,S}
+3    [Cd,Cdd,Ct,Cb,Cbf,O2s,CO,H] u0 {1,S}
+4    Cs                          u0 {1,S}
+5    Cs                          u0 {1,S}
+""",
+    solute = None,
+)
+
+
+entry(
+    index = 10,
+    label = "CsCs-TQ",
+    group = 
+"""
+1 *1 Cs                          u0 {2,S} {3,S} {4,S} {5,S}
+2 *2 Cs                          u0 {1,S} {6,S} {7,S} {8,S}
+3    Cs                          u0 {1,S}
+4    Cs                          u0 {1,S}
+5    [Cd,Cdd,Ct,Cb,Cbf,O2s,CO,H] u0 {1,S}
+6    Cs                          u0 {2,S}
+7    Cs                          u0 {2,S}
+8    Cs                          u0 {2,S}
+""",
+    solute = SoluteData(
+        S = -0.031237301888106923,
+        B = -0.012657889486222615,
+        E = 0.016378468036094582,
+        L = 0.04778412101791641,
+        A = -0.005957763871491428,
+    ),
+    )
+
+
+entry(
+    index = 0,
+    label = "CsCsCs-TQ",
+    group = 
+"""
+1 *1 Cs                          u0 {3,S} {4,S} {5,S} {9,S}
+2 *2 Cs                          u0 {3,S} {6,S} {7,S} {8,S}
+3    Cs                          u0 {1,S} {2,S}
+4    Cs                          u0 {1,S}
+5    Cs                          u0 {1,S}
+6    Cs                          u0 {2,S}
+7    Cs                          u0 {2,S}
+8    Cs                          u0 {2,S}
+9    [Cd,Cdd,Ct,Cb,Cbf,O2s,CO,H] u0 {1,S}
+""",
+    solute = SoluteData(
+        S = 0.019626475933178626,
+        B = -0.005760292597594357,
+        E = -0.006921128971666045,
+        L = 0.04687958565375387,
+        A = -0.008709859216974739,
+    ),
+    )
+
+
+entry(
+    index = 0,
+    label = "CsCsCs",
+    group = 
+"""
+1 *2 Cs u0 {3,S} {4,S} {5,S} {6,S}
+2 *1 Cs u0 {3,S} {7,S} {8,S}
+3    Cs u0 {1,S} {2,S}
+4    Cs u0 {1,S}
+5    Cs u0 {1,S}
+6    Cs u0 {1,S}
+7    Cs u0 {2,S}
+8    Cs u0 {2,S}
+""",
+    solute = None,
+)
+
+
+entry(
+    index = 0,
+    label = "int15",
+    group = 
+"""
+1 *2 Cs           u0 {3,S} {4,S} {5,S} {6,S}
+2 *1 Cs           u0 {3,S} {7,S} {8,S}
+3    [Cs,O2s,S2s] u0 {1,S} {2,S}
+4    Cs           u0 {1,S}
+5    Cs           u0 {1,S}
+6    Cs           u0 {1,S}
+7    Cs           u0 {2,S}
+8    Cs           u0 {2,S}
+""",
+    solute = None,
+)
+
+
+entry(
+    index = 9,
+    label = "CsCs-T(TTP)",
+    group = 
+"""
+1  *1 Cs                          u0 {2,S} {3,S} {4,S} {5,S}
+2  *2 Cs                          u0 {1,S} {6,S} {7,S} {8,S}
+3     Cs                          u0 {1,S} {9,S} {10,S} {11,S}
+4     Cs                          u0 {1,S} {12,S} {13,S} {14,S}
+5     [Cd,Cdd,Ct,Cb,Cbf,O2s,CO,H] u0 {1,S}
+6     Cs                          u0 {2,S}
+7     Cs                          u0 {2,S}
+8     [Cd,Cdd,Ct,Cb,Cbf,O2s,CO,H] u0 {2,S}
+9     Cs                          u0 {3,S}
+10    Cs                          u0 {3,S}
+11    [Cd,Cdd,Ct,Cb,Cbf,O2s,CO,H] u0 {3,S}
+12    [Cd,Cdd,Ct,Cb,Cbf,O2s,CO,H] u0 {4,S}
+13    [Cd,Cdd,Ct,Cb,Cbf,O2s,CO,H] u0 {4,S}
+14    [Cd,Cdd,Ct,Cb,Cbf,O2s,CO,H] u0 {4,S}
+""",
+    solute = SoluteData(
+        S = 0.008101186963055817,
+        B = -0.007486576367143617,
+        E = 0.003608990378498489,
+        L = -0.0835866366105008,
+        A = 0.008910441867079214,
+    ),
+    )
+
+
+entry(
+    index = 11,
+    label = "CsCs-QQ",
+    group = 
+"""
+1 *1 Cs u0 {2,S} {3,S} {4,S} {5,S}
+2 *2 Cs u0 {1,S} {6,S} {7,S} {8,S}
+3    Cs u0 {1,S}
+4    Cs u0 {1,S}
+5    Cs u0 {1,S}
+6    Cs u0 {2,S}
+7    Cs u0 {2,S}
+8    Cs u0 {2,S}
+""",
+    solute = SoluteData(
+        S = -0.010353365105690855,
+        B = -0.02374840267107539,
+        E = 0.0250682525518633,
+        L = 0.015017287224781269,
+        A = -0.014874379132134281,
+    ),
+    )
+
+
+entry(
+    index = 2,
+    label = "CsCs-Q",
+    group = 
+"""
+1 *1 Cs u0 {2,S} {3,S} {4,S} {5,S}
+2 *2 Cs u0 {1,S}
+3    Cs u0 {1,S}
+4    Cs u0 {1,S}
+5    Cs u0 {1,S}
+""",
+    solute = None,
+)
+
+
+entry(
+    index = 9,
+    label = "CsCs-T(TTS)",
+    group = 
+"""
+1  *1 Cs                          u0 {2,S} {3,S} {4,S} {5,S}
+2  *2 Cs                          u0 {1,S} {6,S} {7,S} {8,S}
+3     Cs                          u0 {1,S} {9,S} {10,S} {11,S}
+4     Cs                          u0 {1,S} {12,S} {13,S} {14,S}
+5     [Cd,Cdd,Ct,Cb,Cbf,O2s,CO,H] u0 {1,S}
+6     Cs                          u0 {2,S}
+7     Cs                          u0 {2,S}
+8     [Cd,Cdd,Ct,Cb,Cbf,O2s,CO,H] u0 {2,S}
+9     Cs                          u0 {3,S}
+10    Cs                          u0 {3,S}
+11    [Cd,Cdd,Ct,Cb,Cbf,O2s,CO,H] u0 {3,S}
+12    Cs                          u0 {4,S}
+13    [Cd,Cdd,Ct,Cb,Cbf,O2s,CO,H] u0 {4,S}
+14    [Cd,Cdd,Ct,Cb,Cbf,O2s,CO,H] u0 {4,S}
+""",
+    solute = SoluteData(
+        S = -0.013418809185821815,
+        B = 0.004302057075499487,
+        E = 0.006176547662998522,
+        L = -0.032658257005578395,
+        A = 0.006976124102735123,
+    ),
+    )
+
+
+entry(
+    index = 0,
+    label = "CsCsCs-QQ",
+    group = 
+"""
+1 *1 Cs u0 {3,S} {4,S} {5,S} {6,S}
+2 *2 Cs u0 {3,S} {7,S} {8,S} {9,S}
+3    Cs u0 {1,S} {2,S}
+4    Cs u0 {1,S}
+5    Cs u0 {1,S}
+6    Cs u0 {1,S}
+7    Cs u0 {2,S}
+8    Cs u0 {2,S}
+9    Cs u0 {2,S}
+""",
+    solute = SoluteData(
+        S = -0.0006068562741274098,
+        B = 0.005289569336622016,
+        E = 0.02253674982375654,
+        L = -0.09292404526961788,
+        A = 0.010872534371186137,
+    ),
+    )
+
+
+entry(
+    index = 21,
+    label = "CdCs-ST",
+    group = 
+"""
+1 *2 Cs                          u0 {2,S} {3,S} {4,S} {5,S}
+2 *1 Cd                          u0 {1,S} {6,D} {7,S}
+3    Cs                          u0 {1,S}
+4    Cs                          u0 {1,S}
+5    [Cd,Cdd,Ct,Cb,Cbf,O2s,CO,H] u0 {1,S}
+6    Cd                          u0 {2,D}
+7    Cs                          u0 {2,S}
+""",
+    solute = SoluteData(
+        S = -0.002943244604833988,
+        B = -0.025645988647577708,
+        E = -0.014980784490995845,
+        L = -0.09699644252617191,
+        A = 0.0018269375178330785,
+    ),
+    )
+
+
+entry(
+    index = 19,
+    label = "CdCs-S",
+    group = 
+"""
+1 *1 Cd u0 {2,D} {3,S} {4,S}
+2    Cd u0 {1,D}
+3    Cs u0 {1,S}
+4 *2 Cs u0 {1,S}
+""",
+    solute = None,
+)
+
+
+entry(
+    index = 18,
+    label = "CdCs",
+    group = 
+"""
+1 *1 Cd u0 {2,D} {3,S}
+2    Cd u0 {1,D}
+3 *2 Cs u0 {1,S}
+""",
+    solute = None,
+)
+
+
+entry(
+    index = 22,
+    label = "CdCs-SQ",
+    group = 
+"""
+1 *2 Cs u0 {2,S} {3,S} {4,S} {5,S}
+2 *1 Cd u0 {1,S} {6,D} {7,S}
+3    Cs u0 {1,S}
+4    Cs u0 {1,S}
+5    Cs u0 {1,S}
+6    Cd u0 {2,D}
+7    Cs u0 {2,S}
+""",
+    solute = SoluteData(
+        S = -0.001371991100968279,
+        B = -0.00047004147745950957,
+        E = 0.002306007932551771,
+        L = 0.046441058941177726,
+        A = -0.001624705490922527,
+    ),
+    )
+
+
+entry(
+    index = 16,
+    label = "OsCs-ST",
+    group = 
+"""
+1 *2 Cs                          u0 {2,S} {3,S} {4,S} {5,S}
+2 *1 O2s                         u0 {1,S} {6,S}
+3    Cs                          u0 {1,S}
+4    Cs                          u0 {1,S}
+5    [Cd,Cdd,Ct,Cb,Cbf,O2s,CO,H] u0 {1,S}
+6    Cs                          u0 {2,S}
+""",
+    solute = SoluteData(
+        S = 0.019930737888599316,
+        B = 0.06224825405374003,
+        E = -0.030584509317761667,
+        L = -0.048146133962393624,
+        A = 0.01301313843871556,
+    ),
+    )
+
+
+entry(
+    index = 14,
+    label = "OsCs-S",
+    group = 
+"""
+1 *1 O2s u0 {2,S} {3,S}
+2 *2 Cs  u0 {1,S}
+3    Cs  u0 {1,S}
+""",
+    solute = None,
+)
+
+
+entry(
+    index = 12,
+    label = "OsCs",
+    group = 
+"""
+1 *1 O2s u0 {2,S}
+2 *2 Cs  u0 {1,S}
+""",
+    solute = None,
+)
+
+
+entry(
+    index = 17,
+    label = "OsCs-SQ",
+    group = 
+"""
+1 *2 Cs  u0 {2,S} {3,S} {4,S} {5,S}
+2 *1 O2s u0 {1,S} {6,S}
+3    Cs  u0 {1,S}
+4    Cs  u0 {1,S}
+5    Cs  u0 {1,S}
+6    Cs  u0 {2,S}
+""",
+    solute = SoluteData(
+        S = -0.040412571447463574,
+        B = -0.0025416952891898228,
+        E = -0.027780784015045586,
+        L = 0.1514195075463361,
+        A = 0.0023940218128495824,
+    ),
+    )
+
+
+entry(
+    index = 0,
+    label = "CsOsCs-TQ",
+    group = 
+"""
+1 *1 Cs                          u0 {3,S} {4,S} {5,S} {9,S}
+2 *2 Cs                          u0 {3,S} {6,S} {7,S} {8,S}
+3    O2s                         u0 {1,S} {2,S}
+4    Cs                          u0 {1,S}
+5    Cs                          u0 {1,S}
+6    Cs                          u0 {2,S}
+7    Cs                          u0 {2,S}
+8    Cs                          u0 {2,S}
+9    [Cd,Cdd,Ct,Cb,Cbf,O2s,CO,H] u0 {1,S}
+""",
+    solute = SoluteData(
+        S = -0.012724457947324441,
+        B = -0.00236835867415218,
+        E = -0.04249538703080918,
+        L = -0.05728688998784159,
+        A = 0.0005856302302300942,
+    ),
+    )
+
+
+entry(
+    index = 0,
+    label = "CsOsCs",
+    group = 
+"""
+1 *2 Cs  u0 {3,S} {4,S} {5,S} {6,S}
+2 *1 Cs  u0 {3,S} {7,S} {8,S}
+3    O2s u0 {1,S} {2,S}
+4    Cs  u0 {1,S}
+5    Cs  u0 {1,S}
+6    Cs  u0 {1,S}
+7    Cs  u0 {2,S}
+8    Cs  u0 {2,S}
+""",
+    solute = None,
+)
+
+
+entry(
+    index = 0,
+    label = "CsSsCs-QQ",
+    group = 
+"""
+1 *1 Cs  u0 {3,S} {4,S} {5,S} {6,S}
+2 *2 Cs  u0 {3,S} {7,S} {8,S} {9,S}
+3    S2s u0 {1,S} {2,S}
+4    Cs  u0 {1,S}
+5    Cs  u0 {1,S}
+6    Cs  u0 {1,S}
+7    Cs  u0 {2,S}
+8    Cs  u0 {2,S}
+9    Cs  u0 {2,S}
+""",
+    solute = SoluteData(
+        S = 0.034769555646506835,
+        B = 0.00539369654668091,
+        E = -0.02492683785824779,
+        L = -0.004641064500949273,
+        A = 0.01811015457386952,
+    ),
+    )
+
+
+entry(
+    index = 0,
+    label = "CsSsCs",
+    group = 
+"""
+1 *2 Cs  u0 {3,S} {4,S} {5,S} {6,S}
+2 *1 Cs  u0 {3,S} {7,S} {8,S}
+3    S2s u0 {1,S} {2,S}
+4    Cs  u0 {1,S}
+5    Cs  u0 {1,S}
+6    Cs  u0 {1,S}
+7    Cs  u0 {2,S}
+8    Cs  u0 {2,S}
+""",
+    solute = None,
+)
+
+
+tree(
+"""
+L1: R
+	L2: int15
+		L3: CsSsCs
+			L4: CsSsCs-QQ
+		L3: CsOsCs
+			L4: CsOsCs-TQ
+		L3: CsCsCs
+			L4: CsCsCs-QQ
+			L4: CsCsCs-TQ
+	L2: int14_gauche
+		L3: OsCs
+			L4: OsCs-S
+				L5: OsCs-SQ
+				L5: OsCs-ST
+		L3: CdCs
+			L4: CdCs-S
+				L5: CdCs-SQ
+				L5: CdCs-ST
+		L3: CsCs
+			L4: CsCs-Q
+				L5: CsCs-QQ
+			L4: CsCs-T
+				L5: CsCs-TQ
+				L5: CsCs-TT
+					L6: CsCs-T(TTS)
+					L6: CsCs-T(TTP)
+			L4: CsCs-S
+				L5: CsCs-SQ
+				L5: CsCs-ST
+""",
+)
